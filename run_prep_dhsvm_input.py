e276da09e9372f1551bcff04be9bc96d00dee058
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-02-15 17:05:10 -0800

    script runs, but masks aren't created

--- run_prep_dhsvm_input.py
+++ run_prep_dhsvm_input.py
@@ -9,20 +9,24 @@ import zipfile
 import configparser
 import json
 
+import settings
 
 def main(argv):
-    mask = False
     dhsvm_input_config = False
-    dhsvm_build_path = False
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    path = root_path
+    dhsvm_build_path = settings.DHSVM_BUILD
+    basin_name = settings.DEFAULT_BASIN_NAME
+    mask = settings.SOURCE_INPUT[basin_name]['inputs']['mask']
+    num_cores = 2
+    # root_path = os.path.dirname(os.path.abspath(__file__))
+    # path = root_path
     help_text = "usage: run_prep_dhsvm_input.py -m <path to the mask shp> -i <absolute path to basin's DHSVM input config file> -d <absolute path to DHSVM build directory>"
 
     try:
-        opts, args = getopt.getopt(argv, "hm:i:d:", ["mask=", "dhsvm_input_config=", "dhsvm_build_path="])
+        opts, args = getopt.getopt(argv, "hm:i:d:b:n:", ["mask=", "dhsvm_input_config=", "dhsvm_build_path=","basin_name=","num_cores="])
     except getopt.GetoptError:
         print(help_text)
         sys.exit(2)
+
 
     for (opt, arg) in opts:
         if opt == "-h":
@@ -32,8 +36,16 @@ def main(argv):
             mask = arg
         elif opt in ("-i", "--dhsvm_input_config"):
             dhsvm_input_config = arg
-        elif opt in ("-d", "--dhsvm_build_path"):
-            dhsvm_build_path = arg
+        elif opt in ("-b", "--basin"):
+            basin_name = arg
+        elif opt in ("-n", "--cores"):
+            num_cores = arg
+        # elif opt in ("-w", "--weather"):
+        #     weather_type = arg
+        # elif opt in ("-p", "--rx", "--prescription"):
+        #     prescription = arg
+        # elif opt in ("-d", "--dhsvm_build_path"):
+        #     dhsvm_build_path = arg
 
 
     # Get Input Files and Directories for the Basin
@@ -41,7 +53,7 @@ def main(argv):
 
     # check for the input config file
     try:
-        basin_path = os.path.dirname(dhsvm_input_config)
+        run_dir = os.path.dirname(dhsvm_input_config)
     except FileNotFoundError as e:
         print(
             "Path not found: %s. Please provide an absolute path to the your DHSVM input config file"
@@ -49,27 +61,27 @@ def main(argv):
         sys.exit()
 
     # the original basin bin files directory
-    basin_orig_input_files_dir = os.path.join(basin_path, "orig_inputs")
+    basin_orig_input_files_dir = os.path.join(run_dir, "inputs")
     try:
         basin_orig_input_files = os.listdir(basin_orig_input_files_dir)
     except FileNotFoundError as e:
         print(
             "Path not found: %s. Please provide an 'orig_inputs' directory with required original input files for your basin."
-            % basin_path
+            % run_dir
         )
         sys.exit()
 
     # the inputs directory and files
-      # this is where we store files changed by the rest of this script and feed into dhsvm
-    basin_working_input_file_dir = os.path.join(basin_path, "inputs")
-    try:
-        basin_working_input_files = os.listdir(basin_working_input_file_dir)
-    except FileNotFoundError as e:
-        print(
-            "Path not found: %s. Please provide an 'inputs' directory with required input files for your basin."
-            % basin_path
-        )
-        sys.exit()
+    #   this is where we store files changed by the rest of this script and feed into dhsvm
+    # basin_working_input_file_dir = os.path.join(run_dir, "inputs")
+    # try:
+    #     basin_working_input_files = os.listdir(basin_working_input_file_dir)
+    # except FileNotFoundError as e:
+    #     print(
+    #         "Path not found: %s. Please provide an 'inputs' directory with required input files for your basin."
+    #         % run_dir
+    #     )
+    #     sys.exit()
 
     # Is mask provided
     try:
@@ -107,15 +119,15 @@ def main(argv):
             print("desired yllcorner not found in mask header")
 
 
-    if not dhsvm_build_path:
-        dhsvm_build_path = '/storage/DHSVM_2020/DHSVM-PNNL/build'
+    # if not dhsvm_build_path:
+    #     dhsvm_build_path = '/storage/DHSVM_2020/DHSVM-PNNL/build'
 
 
     # Check for DHSVM dependencies
     ##############################
 
     # Model state
-    basin_modelstate_dir = os.path.join(basin_working_input_file_dir, "modelstate")
+    basin_modelstate_dir = os.path.join(basin_orig_input_files_dir,'modelstate')
     try:
         os.path.isdir(basin_modelstate_dir)
     except OSError:
@@ -123,7 +135,7 @@ def main(argv):
         sys.exit()
 
     # Shadows
-    basin_shadows_dir = os.path.join(basin_path, "shadows")
+    basin_shadows_dir = os.path.join(run_dir,'shadows')
     try:
         os.path.isdir(basin_shadows_dir)
     except OSError:
@@ -131,17 +143,18 @@ def main(argv):
         sys.exit()
 
     # Weather data
-    basin_livneh_dir = os.path.join(basin_path, "livneh_1950_2013")
+    met_data_dir = os.path.join(run_dir,'met_data')
     try:
-        os.path.isdir(basin_livneh_dir)
+        os.path.isdir(met_data_dir)
     except OSError:
-        print("Path not found: %s. Please provide a livneh_1950_2013 dir" % basin_livneh_dir)
+        print("Path not found: %s. Please provide meteorological data directory" % met_data_dir)
         sys.exit()
 
     # Output dir
-    basin_output_dir = os.path.join(basin_path, "output")
-    if not os.path.isdir(basin_output_dir):
-        os.system("mkdir %s" % basin_output_dir)
+    run_output_dir = os.path.join(run_dir, "output")
+    if os.path.isdir(run_output_dir):
+        shutil.rmtree(run_output_dir)
+    os.mkdir(run_output_dir)
 
 
     # Parse input config file
@@ -166,46 +179,47 @@ def main(argv):
     ######################
 
     # path to myconvert
-    myconvert = os.path.join(dhsvm_build_path, 'DHSVM/program/myconvert')
+    myconvert = os.path.join(dhsvm_build_path, 'DHSVM', 'program', 'myconvert')
 
     # myconvert conversion type for each data file
-    data_types = {
-        "dem": "float",
-        "dir": "character",
-        "mask": "character",
-        "soild": "character",
-        "soiltype": "character",
-        "veg": "character",
-    }
+    # ASSUPTION: basin name never contains '_'
 
+    data_types = {
+        "_dem": "float",
+        "_dir": "character",
+        "_mask": "character",
+        "_soild": "character",
+        "_soiltype": "character",
+        "_veg": "character",
+    }
 
     # Now create the rest of the data files
     for input in basin_orig_input_files:
         input_extension = os.path.splitext(input)[-1]
         if input_extension == '.bin':
             # splittext gives us the name bc its form ('<data>.asc', '.bin')
-            input_asc_name = os.path.splitext(input)[0]
-            input_asc_path = os.path.abspath(os.path.join(basin_working_input_file_dir, input_asc_name))
-            input_bin_path = os.path.abspath(os.path.join(basin_orig_input_files_dir, input))
+            unmasked_asc_name = os.path.splitext(input)[0]
+            unmasked_asc_path = os.path.abspath(os.path.join(basin_orig_input_files_dir, unmasked_asc_name))
+            unmasked_bin_path = os.path.abspath(os.path.join(run_dir, input))
             for key in data_types:
                 if key in input:
                     use_type = data_types[key]
             os.system(
                 "%s %s ascii %s %s %s %s"
-                % (myconvert, use_type, input_bin_path, input_asc_path, number_of_rows, number_of_columns)
+                % (myconvert, use_type, unmasked_bin_path, unmasked_asc_path, number_of_rows, number_of_columns)
             )
-            print("myconvert to ascii \npath: %s \ntype: %s \noutput: %s" % (input_bin_path, use_type, input_asc_path))
+            print("myconvert to ascii \npath: %s \ntype: %s \noutput: %s" % (unmasked_bin_path, use_type, unmasked_asc_path))
 
             # Save full basin mask path for later
-            if "mask" in input_asc_name:
-                entire_basin_mask = input_asc_path
+            # if "mask" in unmasked_asc_name:
+            #     entire_basin_mask = unmasked_asc_path
 
     # check that entire basin mask file is created
-    try:
-        os.path.isfile(entire_basin_mask)
-    except OSError:
-        print("Missing mask of entire basin: %s \nCheck that a mask file is in the orig_inputs directory" % entire_basin_mask)
-        sys.exit()
+    # try:
+    #     os.path.isfile(entire_basin_mask)
+    # except OSError:
+    #     print("Missing mask of entire basin: %s \nCheck that a mask file is in the orig_inputs directory" % entire_basin_mask)
+    #     sys.exit()
 
 
     # Add Header to ascii files
@@ -213,10 +227,10 @@ def main(argv):
 
     ascii_header = "ncols        %s\nnrows        %s\nxllcorner    %s\nyllcorner    %s\ncellsize     %s\nNODATA_value  %s\n" % (number_of_rows, number_of_columns, xllcorner, yllcorner, cellsize, NODATA_value)
 
-    for input in basin_working_input_files:
-        input_extension = os.path.splitext(input)[-1]
+    for input_file in basin_orig_input_files:
+        input_extension = os.path.splitext(input_file)[-1]
         if input_extension == '.asc':
-            input_path = os.path.abspath(os.path.join(basin_working_input_file_dir, input))
+            input_path = os.path.abspath(os.path.join(basin_orig_input_files_dir, input_file))
             ascii_file = open(input_path, "r+")
             content = ascii_file.read()
             ascii_file.seek(0,0)
@@ -227,7 +241,7 @@ def main(argv):
     # Overwrite full basin mask with desired mask
     #############################################
 
-    os.system('cp %s %s' % (mask, entire_basin_mask))
+    # os.system('cp %s %s' % (mask, entire_basin_mask))
 
 
     # Add crs to ascii files
@@ -250,6 +264,11 @@ def main(argv):
     # Create the mask polygon
     #########################
 
+    masked_dir = os.path.join(basin_orig_input_files_dir, 'masked')
+    if os.path.isdir(masked_dir):
+        shutil.rmtree(masked_dir)
+    os.mkdir(masked_dir)
+
     if ".shp" in mask:
         import fiona
 
@@ -260,7 +279,7 @@ def main(argv):
         sys.exit()
 
     else:
-        mask_feature = os.path.join(basin_working_input_file_dir, "basin.geojson")
+        mask_feature = os.path.join(masked_dir, "basin.geojson")
 
         # Convert ascii to geojson feature
         os.system("rio shapes --projected %s > %s" % (mask, mask_feature))
@@ -297,37 +316,32 @@ def main(argv):
     # Mask
     ######
 
-    for input in basin_working_input_files:
-        input_extension = os.path.splitext(input)[-1]
+    for unmasked_file in basin_orig_input_files_dir:
+        input_extension = os.path.splitext(unmasked_file)[-1]
         if input_extension == ".asc":
-            mask_feature = os.path.join(basin_working_input_file_dir, "basin.geojson")
-            input_name = os.path.splitext(input)[0]
-            input_path = os.path.abspath(os.path.join(basin_working_input_file_dir, input))
-            masked_dir = os.path.join(basin_working_input_file_dir, "masked")
-            if not os.path.isdir(masked_dir):
-                os.system("mkdir %s" % masked_dir)
-            output_masked = os.path.abspath(os.path.join(masked_dir, input))
+            mask_feature = os.path.join(masked_dir, "basin.geojson")
+            input_name = os.path.splitext(unmasked_file)[0]
+            input_path = os.path.abspath(os.path.join(basin_orig_input_files_dir, unmasked_file))
+            output_masked = os.path.abspath(os.path.join(masked_dir, unmasked_file))
             os.system("rio mask %s %s --crop --overwrite --geojson-mask %s" % (input_path, output_masked, mask_feature))
-
-    # overwrite the original entire basin mask with the desired mask
 
 
     # Remove header from ascii
     ##########################
-    basin_masked_input_dir = os.path.abspath(os.path.join(basin_working_input_file_dir, "masked"))
     try:
-        basin_masked_files = os.listdir(basin_masked_input_dir)
+        basin_masked_files = os.listdir(masked_dir)
     except OSError:
         print(
             "Path not found: %s. Please make a masked directory in your basins inputs directory."
-            % basin_masked_input_dir
+            % masked_dir
         )
         sys.exit()
 
-    for input in basin_working_input_files:
-        input_extension = os.path.splitext(input)[-1]
+    # Off with their heads!
+    for masked_file in masked_dir:
+        input_extension = os.path.splitext(masked_file)[-1]
         if input_extension == '.asc':
-            input_path = os.path.abspath(os.path.join(basin_masked_input_dir, input))
+            input_path = os.path.abspath(os.path.join(masked_dir, masked_file))
             ascii_file = open(input_path, "r+")
             content_lines = ascii_file.readlines()
             ascii_file.seek(0,0)
@@ -341,18 +355,18 @@ def main(argv):
     # Convert ascii to bin
     ######################
 
-    for input in basin_masked_files:
-        input_extension = os.path.splitext(input)[-1]
+    for masked_file in basin_masked_files:
+        input_extension = os.path.splitext(masked_file)[-1]
         if input_extension == '.asc':
-            input_bin_name = os.path.splitext(input)[0] + ".asc.bin"
-            input_bin_path = os.path.abspath(os.path.join(basin_working_input_file_dir, input_bin_name))
-            input_masked_path = os.path.abspath(os.path.join(basin_masked_input_dir, input))
+            masked_bin_name = os.path.splitext(masked_file)[0] + ".asc.bin"
+            masked_bin_path = os.path.abspath(os.path.join(masked_dir, masked_bin_name))
+            masked_ascii_path = os.path.abspath(os.path.join(masked_dir, masked_file))
             for key in data_types:
-                if key in input:
+                if key in masked_file:
                     use_type = data_types[key]
             os.system(
                 "%s ascii %s %s %s %s %s"
-                % (myconvert, use_type, input_masked_path, input_bin_path, mask_nrows, mask_ncols)
+                % (myconvert, use_type, masked_ascii_path, masked_bin_path, mask_nrows, mask_ncols)
             )
 
 
@@ -364,13 +378,13 @@ def main(argv):
     input_config.set("AREA", "Extreme West", mask_xllcorner)
     input_config.set("AREA", "Extreme North", mask_yllcorner)
 
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
 
     # Run DHSVM
     ###########
 
-    dhsvm_path = os.path.join(dhsvm_build_path, 'DHSVM/sourcecode/DHSVM')
-    os.system("%s %s" % (dhsvm_path, dhsvm_input_config))
+    dhsvm_path = os.path.join(dhsvm_build_path, 'DHSVM','sourcecode','DHSVM')
+    os.system("mpiexec -n %s %s %s" % (num_cores, dhsvm_path, dhsvm_input_config))
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 BASINS = ['entiat', 'methow', 'okan', 'wena']
+DEFAULT_BASIN_NAME = 'entiat'
 
 from local_settings import *
